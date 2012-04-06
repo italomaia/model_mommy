8ded66c6de772cc46357afb073eafacc52b56956
italo maia <italo.maia@gmail.com>
2012-04-06 05:58:30 -0300

    Adicionada documentação e testes inline

--- model_mommy/base.py
+++ model_mommy/base.py
@@ -7,6 +7,7 @@ from django.db.models.fields.files import *
 from .utils import *
 
 import datetime
+from random import random
 
 # ref: http://docs.python.org/howto/unicode.html
 UNICODE_RANGE = (0, 1114111)
@@ -30,17 +31,30 @@ class Base(object):
         self.model = model
 
     def make(self, **attrs):
-        return self._make(True, **attrs)
+        """
+        Makes one instance of the registered model. (commits instance)
+
+        """
+        return self.__make(True, **attrs)
 
     def prepare(self, **attrs):
-        return self._make(False, **attrs)
+        """
+        Prepares one instance of the registered model. (does not commit instance)
 
-    def _make(self, commit, **attrs):
+        """
+        return self.__make(False, **attrs)
+
+    def get_fields(self):
+        """
+        Returns all available fields (regular fields plus m2m fields)
+
+        """
+        return self.model._meta.fields + self.model._meta.many_to_many
+
+    def __make(self, commit, **attrs):
         for field in self.get_fields():
-            if isinstance(field, AutoField):  # auto populated
-                continue
             # field value was provided. Ignoring...
-            elif field.name in attrs:
+            if field.name in attrs:
                 continue
             elif type(field) in (OneToOneField, ForeignKey, ManyToManyField) and field.null:
                 continue
@@ -49,12 +63,9 @@ class Base(object):
             elif field.blank and choice(LEAVE_TO_CHANCE):
                 attrs[field.name] = ''
             else:
-                attrs[field.name] = self.get_value_for_field(field)
+                attrs[field.name] = self.__get_value_for_field(field)
 
-    def get_fields(self):
-        return self.model._meta.fields + self.model._meta.many_to_many
-
-    def get_value_for_field(self, field):
+    def __get_value_for_field(self, field):
         field_cls = field.__class__
         field_cls_name = field_cls.__name__
 
@@ -67,31 +78,81 @@ class Base(object):
         else:  # unsupported field type
             raise TypeError('%s is not supported by mommy.' % field_cls_name)
 
+    def value_for_autofield(self, field):
+        return None
+
     def value_for_booleanfield(self, field):
+        """
+        Returns True or False
+
+        """
         return choice((False, True))
 
     def value_for_nullbooleanfield(self, field):
+        """
+        Returns None, True or False
+
+        """
         return choice((None, False, True))
 
     def value_for_smallintegerfield(self, field):
         return randint(MIN_SMALL_INT, MAX_SMALL_INT)
 
     def value_for_positivesmallintegerfield(self, field):
+        """
+        Returns a positive 16bits integer
+
+        >>> field = object()
+        >>> base = Base({})
+        >>> value = base.value_for_positivesmallintegerfield(field)
+        >>> assert value >= 0
+        """
         return randint(0, MAX_SMALL_INT)
 
     def value_for_integerfield(self, field):
         return randint(MIN_INT, MAX_INT)
 
     def value_for_positiveintegerfield(self, field):
+        """
+        Returns a positive 32bits integer
+
+        >>> field = object()
+        >>> base = Base({})
+        >>> value = base.value_for_positiveintegerfield(field)
+        >>> assert value >= 0
+        """
         return randint(0, MAX_INT)
 
     def value_for_bigintegerfield(self, field):
         return randint(MIN_BIG_INT, MAX_BIG_INT)
 
     def value_for_floatfield(self, field):
+        """
+        Returns a random float value
+
+        >>> field = object()
+        >>> base = Base({})
+        >>> value = base.value_for_floatfield(field)
+        >>>
+        >>> assert isinstance(value, float)
+        """
         return random() * randint(MIN_INT, MAX_INT)
 
     def value_for_decimalfield(self, field):
+        """
+        Returns a random decimal like string obeying field's max_digits and decimal_places
+
+        >>> field = object()
+        >>> field.max_digits = 10
+        >>> field.decimal_places = 2
+        >>> base = Base({})
+        >>> value = base.value_for_decimalfield(field)
+        >>> dec, frac = value.split(".")
+        >>>
+        >>> assert len(dec) + len(frac) <= field.max_digits
+        >>> assert len(frac) <= field.decimal_places
+        >>> assert dec.isdigit() or frac.isdigit()
+        """
         md, dp = field.max_digits, field.decimal_places
         dp_length = randint(0, dp)
         md_length = md - dp_length
@@ -100,12 +161,35 @@ class Base(object):
         return "%s.%s" % (md_number, dp_number)
 
     def value_for_commaseparatedintegerfield(self, field):
-        max_length = field.max_length
-        str_int_list = ""
+        """
+        Returns string in the format number[,number]*
+        *number* is a 32bits integer
 
-        while len(str_int_list) < max_length:
-            str_int_list += not choice(LEAVE_TO_CHANCE) and str(randint(0, 9)) or ","
-        return str_int_list
+        >>> import re
+        >>> c = re.compile('^\d+(m\d+)$')
+        >>> field = object()
+        >>> field.max_length = 10
+        >>> base = Base({})
+        >>> value = base.value_for_commaseparatedintegerfield(field)
+        >>>
+        >>> assert c.match(value) is not None
+        >>> assert len(value) <= field.max_value
+        """
+        max_length = field.max_length
+
+        number = randint(MIN_INT, MAX_INT)
+        str_number = str(number)
+
+        rt = str(int(str_number[min(len(str_number), max_length):]))
+
+        while (len(rt) < max_length - 1) and choice(True, True, False):
+            number = randint(MIN_INT, MAX_INT)
+            str_number = str(number)
+
+            rt += ","
+            rt += str(int(str_number[min(len(str_number), max_length - len(rt)):]))
+
+        return rt
 
     def value_for_datefield(self, field):
         """
@@ -159,18 +243,26 @@ class Base(object):
         return '.'.join([str(token) for token in ip_address])
 
     def value_for_charfield(self, field):
+        """
+        Creates a random string with provided max_length
+
+        """
         max_length = field.max_length
         length = randint(1, max_length)
-        return raw_string(length, LATIN1_TABLE)
+        return raw_string(length, UNICODE_RANGE)
 
     def value_for_slugfield(self, field):
+        """
+        Creates a random slug field with provided max_length
+
+        """
         max_length = field.max_length
         length = randint(1, max_length)
         return raw_string(length, SLUG_TABLE)
 
     def value_for_textfield(self, field):
         length = randint(1, TEXT_MAX_LENGTH)
-        return raw_string(length, LATIN1_TABLE + "\n")
+        return raw_string(length, UNICODE_RANGE + "\n")
 
     def value_for_xmlfield(self, field):
         pass
